f122a579652e6889c75ea3ea49161ed9ef0437f3
kale-io <cos+a@iuvo.ai>
2019-03-06 22:32:33 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/utils.toUtf8.js
+++ test/utils.toUtf8.js
@@ -9,10 +9,10 @@ var tests = [
     { value: '0x65787065637465642076616c7565000000000000000000000000000000000000', expected: 'expected value'}
 ];
 
-describe('lib/utils/utils', function () {
-    describe('toUtf8', function () {
-        tests.forEach(function (test) {
-            it('should turn ' + test.value + ' to ' + test.expected, function () {
+describe('lib/utils/utils', () => {
+    describe('toUtf8', () => {
+        tests.forEach(test => {
+            it('should turn ' + test.value + ' to ' + test.expected, () => {
                 assert.strictEqual(utils.toUtf8(test.value), test.expected);
             });
         });
