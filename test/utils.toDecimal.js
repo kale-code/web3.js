d0f620d48d1b953464ddca32fd1ae181e570f95c
kale-io <cos+a@iuvo.ai>
2019-03-06 22:32:53 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/utils.toDecimal.js
+++ test/utils.toDecimal.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 var utils = require('../lib/utils/utils.js');
 
-describe('lib/utils/utils', function () {
-    describe('toDecimal', function () {
-        it('should return the correct value', function () {
+describe('lib/utils/utils', () => {
+    describe('toDecimal', () => {
+        it('should return the correct value', () => {
             
             assert.equal(utils.toDecimal("0x3e8"), '1000');
             // allow compatiblity
