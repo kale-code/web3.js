05d54e0a96d4004d2763ddc35f9dee8b8734726b
kale-io <cos+a@iuvo.ai>
2019-03-06 22:39:10 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/iban.toAddress.js
+++ test/iban.toAddress.js
@@ -6,10 +6,10 @@ var tests = [
     { direct: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS', address: '00c5496aee77c1ba1f0854206a26dda82a81d6d8'}
 ];
 
-describe('lib/web3/iban', function () {
-    describe('toAddress', function () {
-        tests.forEach(function (test) {
-            it('shoud transform iban to address: ' +  test.address, function () {
+describe('lib/web3/iban', () => {
+    describe('toAddress', () => {
+        tests.forEach(test => {
+            it('shoud transform iban to address: ' +  test.address, () => {
                 var iban = new Iban(test.direct);
                 assert.deepEqual(iban.address(), test.address);
             });
