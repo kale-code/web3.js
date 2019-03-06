783be78cf60c5f97bcd49cf870c80b36b09cee04
kale-io <cos+a@iuvo.ai>
2019-03-06 22:38:19 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/method.validateArgs.js
+++ test/method.validateArgs.js
@@ -3,9 +3,9 @@ var assert = chai.assert;
 var Method = require('../lib/web3/method');
 var errors = require('../lib/web3/errors');
 
-describe('lib/web3/method', function () {
-    describe('validateArgs', function () {
-        it('should pass', function () {
+describe('lib/web3/method', () => {
+    describe('validateArgs', () => {
+        it('should pass', () => {
             
             // given
             var method = new Method({
@@ -16,15 +16,15 @@ describe('lib/web3/method', function () {
             var args2 = ['heloas'];
 
             // when
-            var test = function () { method.validateArgs(args); };
-            var test2 = function () { method.validateArgs(args2); };
+            var test = () => { method.validateArgs(args); };
+            var test2 = () => { method.validateArgs(args2); };
 
             // then
             assert.doesNotThrow(test);
             assert.doesNotThrow(test2);
         });
 
-        it('should return call based on args', function () {
+        it('should return call based on args', () => {
 
             // given
             var methodName = 'testMethod';
@@ -37,8 +37,8 @@ describe('lib/web3/method', function () {
             var args2 = ['heloas', '12', 3];
 
             // when
-            var test = function () { method.validateArgs(args); };
-            var test2 = function () { method.validateArgs(args2); };
+            var test = () => { method.validateArgs(args); };
+            var test2 = () => { method.validateArgs(args2); };
             
             // then
             assert.throws(test, errors.InvalidNumberOfRPCParams(methodName).message);
